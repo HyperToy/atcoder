570979126da9c900161e3965fcf96018efe46b7b
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-08-30 08:38:51 +0900

    [add] abc265_f not solved
    黄色diff, しばらく先になりそう

---
+++ abc/265/f.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main(){
+    int N, D;
+    cin >> N >> D;
+    vector<int> p(N), q(N);
+    rep(i,N) cin >> p[i];
+    rep(i,N) cin >> q[i];
+
+    
+} 
