854b08c7b4f336a5555a8d24e591c1d97f24d85d
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-08-30 22:40:00 +0900

    [update] abc262_d upsolve

--- abc/262/d.cpp
+++ abc/262/d.cpp
@@ -11,26 +11,22 @@ int main(){
     vector<ll> a(N);
     rep(i,N) cin >> a[i];
 
-    vector<map<ll, ll>> dp(1);
-    dp[0][0] = 1;
-    rep(i,N) {
-        vector<map<ll, ll>> nextDp(i+2);
-        rep(j,i+1) {
-            for (auto p : dp[j]) {
-                (nextDp[j][p.first] += p.second) %= MOD;
-                (nextDp[j+1][p.first + a[i]] += p.second) %= MOD;
-            }
-        }
-        dp = nextDp;
-    }
-
     ll ans = 0;
-    for (int j = 1; j <= N; j++) {
-        for (auto p : dp[j]) {
-            if (p.first % j == 0) {
-                (ans += p.second) %= MOD;
+    for (int i = 1; i <= N; i++) {
+        // dp[j][k][l] := j番目までから k個選んで i で割った余りが l である個数
+        vector dp(N+1, vector<int>(i, 0));
+        dp[0][0] = 1;
+        for (int j = 0; j < N; j++) {
+            vector nextDp(N+1, vector<int>(i, 0));
+            for (int k = 0; k < N; k++) {
+                for (int l = 0; l < i; l++) {
+                    (nextDp[k][l] += dp[k][l]) %= MOD;
+                    (nextDp[k+1][(l + a[j]) % i] += dp[k][l]) %= MOD;
+                }
             }
+            dp = nextDp;
         }
+        (ans += dp[i][0]) %= MOD;
     }
 
     cout << ans << endl;
