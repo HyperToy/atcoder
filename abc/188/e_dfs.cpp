170c2a05af0d870baecad10e06516f17e04fddad
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-09-10 12:26:22 +0900

    [update] abc188_e dfs解法 waが取れない

--- abc/188/e_dfs.cpp
+++ abc/188/e_dfs.cpp
@@ -1,25 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 #define PR(x) cerr << #x << " = " << x << endl
 template<class t,class u> bool chmax(t&a,u b){if(a<b){a=b;return true;}else return false;}
 template<class t,class u> bool chmin(t&a,u b){if(b<a){a=b;return true;}else return false;}
 const int INF = 1001001001;
+const long long LINF = 1001001001001001001;
 
-int dfs(vector<int> &A, vector<vector<int>> &g, vector<bool> &seen, int mn, int pos, int pre = -1) {
+ll dfs(vector<ll> &A, vector<vector<int>> &g, vector<bool> &seen, vector<ll> &backmax, int pos, int pre = -1) {
+    if (seen[pos]) return backmax[pos];
     seen[pos] = true;
-    int res = A[pos] - mn;
+    ll res = -LINF;
     for (int nxt : g[pos]) {
         // if (nxt == pre) continue;
-        chmax(res, dfs(A, g, seen, min(mn, A[pos]), nxt, pos));
+        chmax(res, dfs(A, g, seen, backmax, nxt, pos));
     }
-    return res;
+    backmax[pos] = res;
+    return max(A[pos], backmax[pos]);
 }
 
 int main(){
     int N, M;
     cin >> N >> M;
-    vector<int> A(N);
+    vector<ll> A(N);
     vector<vector<int>> g(N);
     rep(i,N) cin >> A[i];
     rep(_,M) {
@@ -29,12 +33,14 @@ int main(){
         g[X].push_back(Y);
     }
     vector<bool> seen(N, false);
-
-    int ans = -INF;
+    vector<ll> backmax(N, -LINF);
     rep(i,N) {
         if (!seen[i]) {
-            chmax(ans, dfs(A, g, seen, INF, i));
+            dfs(A, g, seen, backmax, i);
         }
     }
+
+    ll ans = -LINF;
+    rep(i,N) chmax(ans, backmax[i] - A[i]);
     cout << ans << endl;
 } 
