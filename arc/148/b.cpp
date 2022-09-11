102ff3ad2672eca3a53a876199ba84327a28caa9
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-09-11 23:08:59 +0900

    [add] arc148_a,b in contest
    A: コーナーケースを考慮できず。
    コーナーケースのパターンとして、「全部同じ」を覚えておく。
    
    B: 難しく考えて実装が爆発した。愚直を忘れない。

---
+++ arc/148/b.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+#define PR(x) cerr << #x << " = " << x << endl
+
+int main(){
+    int N;
+    string S;
+    cin >> N >> S;
+
+    int max_serial_p = 0;
+    int first_p_pos = -1;
+    stack<pair<char, int>> st;
+    rep(i,N) {
+        if (S[i] == 'p' && first_p_pos == -1) {
+            first_p_pos = i;
+        }
+        if (st.size() > 0 && st.top().first == S[i]) {
+            st.top().second++;
+        } else {
+            st.emplace(S[i], 1);
+        }
+        if (st.top().first == 'p') {
+            max_serial_p = max(max_serial_p, st.top().second);
+        }
+    }
+    int offset = 0;
+    stack<int> offsets;
+    while (st.size() > 0) {
+        if (st.top().first == 'p' && st.top().second == max_serial_p) {
+            offsets.push(offset);
+            offset = 0;
+        }
+        offset += st.top().second;
+        st.pop();
+    }
+    if (offsets.size() == 0) {
+        cout << S << endl;
+        return 0;
+    }
+    
+    vector<string> div;
+    div.push_back(S.substr(0, first_p_pos));
+    div.push_back(S.substr(first_p_pos, offset - first_p_pos));
+    while (offsets.size() > 0) {
+        div.push_back(S.substr(offset, offsets.top()));
+        offset += offsets.top();
+        offsets.pop();
+    }
+    for (string s : div) PR(s);
+
+    int d = div.size();
+    vector<stack<pair<char, int>>> vsp(d);
+    rep(i,d) {
+        string s = div[i];
+        rep(j,s.length()) {
+            if (vsp[i].size() > 0 && vsp[i].top().first == s[i]) {
+                vsp[i].top().second++;
+            } else {
+                vsp[i].emplace(s[i], 1);
+            }
+        }
+    }
+    set<int> use;
+    rep(i,d) {
+        if (vsp[i].top().first == 'p') {
+            use.insert(i);
+        }
+    }
+    while (true) {
+        set<int> next_use;
+        int max_p_count = 0;
+        for (int i : use) {
+            if (vsp[i].size() > 0) { 
+                max_p_count  = max(max_p_count, vsp[i].top().second);
+            }
+        }
+        for (int i : use) {
+            if (vsp[i].size() > 0 && vsp[i].top().second == max_p_count) {
+                next_use.insert(i);
+                vsp[i].pop();
+            }
+        }
+        use = next_use;
+        if (use.size() == 1) break;
+
+        int min_d_count = 1001001001;
+        for (int i : use) {
+            if (vsp[i].size() > 0) {
+                min_d_count = min(min_d_count, vsp[i].top().second);
+            }
+        }
+        for (int i : use) {
+            if (vsp[i].size() > 0 && vsp[i].top().second == min_d_count) {
+                next_use.insert(i);
+                vsp[i].pop();
+            }
+        }
+        use = next_use;
+        if (use.size() == 1) break;
+    }
+
+    // 1 ~ *(use.begin()) をフリップ
+    // int L = first_p_pos;
+    
+} 
