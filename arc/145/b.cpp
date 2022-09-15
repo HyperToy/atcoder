a38dd022d0c600a804b9a95989a006f7b49b40f1
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-09-15 10:12:02 +0900

    [update] arc145_b editorial

--- arc/145/b.cpp
+++ arc/145/b.cpp
@@ -3,19 +3,13 @@ using namespace std;
 using ll = long long;
 #define PR(x) cerr << #x << " = " << x << endl
 
+ll f(ll x, ll a, ll b) {
+    return (x / a) * min(a, b) + min(x % a, b - 1);
+}
+
 int main(){
     ll N, A, B;
     cin >> N >> A >> B;
-
-    ll ans;
-    if (A < B) {
-        ll lose = min(N, A - 1);
-        ans = N - lose;
-    } else {
-        ll win = (N / A) * B;
-        win -= min(N, B - 1);
-        win += min(N % A, B - 1);
-        ans = win;
-    }
+    ll ans = max(f(N, A, B) - f(A - 1, A, B), 0LL);
     cout << ans << endl;
 }
