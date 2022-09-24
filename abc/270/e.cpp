cbd63ff0fcc5dc5c169f67db6aa74a12be60845c
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-09-24 23:20:03 +0900

    [update] abc270_e コンテスト終了直後AC

--- abc/270/e.cpp
+++ abc/270/e.cpp
@@ -3,6 +3,8 @@ using namespace std;
 using ll = long long;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 #define PR(x) cerr << #x << " = " << x << endl
+template<class t,class u> bool chmax(t&a,u b){if(a<b){a=b;return true;}else return false;}
+template<class t,class u> bool chmin(t&a,u b){if(b<a){a=b;return true;}else return false;}
 
 int main(){
     int N;
@@ -11,24 +13,28 @@ int main(){
     vector<ll> A(N);
     rep(i,N) cin >> A[i];
 
-    priority_queue<ll, vector<ll>, greater<ll>> q;
+    vector<ll> b = A;
+    sort(b.begin(), b.end());
+    vector<pair<ll, ll>> v;
+    ll pre = 0;
     rep(i,N) {
-        q.push(A[i]);
+        v.emplace_back(N - i, b[i] - pre);
+        pre = b[i];
     }
 
-    ll diff = 0;
     ll total = 0;
-    bool go = true;
-    while (go) {
-        diff = q.top() - total;
-        q.pop();
-        if (K < diff * (q.size() + 1)) {
-            diff = K / (q.size() + 1);
-            go = false;
+    rep(i,N) {
+        if (K >= v[i].first * v[i].second) {
+            K -= v[i].first * v[i].second;
+            total += v[i].second;
+        } else {
+            ll delta = K / v[i].first;
+            K -= v[i].first * delta;
+            total += delta;
+            break;
         }
-        K -= diff * (q.size() + 1);
-        total += diff;
     }
+
     rep(i,N) A[i] = max(0LL, A[i] - total);
     rep(i,N) {
         if (K > 0 && A[i] > 0) {
