65cfacb787bbe26617ff6de92adc64fdfeceb7cc
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-09-28 21:34:53 +0900

    [update] degitalarts2012_a 正規表現を諦めてAC

--- other/digitalarts2012/a.cpp
+++ other/digitalarts2012/a.cpp
@@ -5,23 +5,26 @@ using namespace std;
 int main(){
     string s;
     getline(cin, s);
+    int n = s.length();
 
     int N;
     cin >> N;
     vector<string> t(N);
     rep(i,N) cin >> t[i];
     
-    s = " " + s + " ";
-    rep(i,N) {
-        string reg = " ";
-        int l = t[i].length();
-        rep(j,l) {
-            if (t[i][j] == '*') reg += "[a-z]";
-            else reg += t[i][j];
+    for (string u : t) {
+        int l = u.length();
+        rep(i,n-l+1) {
+            bool ok = true;
+            rep(j,l) {
+                if (s[i+j] == ' ') ok = false;
+                if (u[j] != '*' && u[j] != s[i+j]) ok = false;
+            }
+            if (i != 0 && s[i-1] != ' ') ok = false;
+            if (i + l - 1 != n - 1 && s[i + l] != ' ') ok = false;
+            if (ok) rep(j,l) s[i+j] = '*';
         }
-        reg += " ";
-        s = regex_replace(s, regex(reg), " " + string(l, '*') + " ");
     }
-    s = s.substr(1, s.length() - 2);
-    cout << s << endl;
+
+   cout << s << endl;
 }
