c5a83c88cf0bda6ac0e6a4657e16f9a42a68afc8
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-10-09 10:17:22 +0900

    [update] abc272_e AC with editorial

--- abc/272/e.cpp
+++ abc/272/e.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 
 int main(){
@@ -9,32 +8,33 @@ int main(){
     vector<int> A(N);
     rep(i,N) cin >> A[i];
 
-    vector<set<int>> in(M);
-    vector<set<int>> out(M);
-
-    set<int> concerned;
+    vector<set<int>> block(M);
     rep(i,N) {
-        // A[i] >= 0 になる
-        int j = (-A[i]) / (i + 1);
-        if (1 <= j && j <= M) in[j - 1].insert(i);
-        if (j < 1) concerned.insert(i);
-        // A[i] >= N になる
-        j = (N - A[i]) / (i + 1);
-        if (1 <= j && j <= M) out[j - 1].insert(i);
+        int d = i + 1;
+        if (A[i] < 0) {
+            int j = (-A[i] - 1) / d;
+            A[i] += j * d;
+            for (; j < M; j++) {
+                A[i] += d;
+                if (A[i] >= N) break;
+                block[j].insert(A[i]);
+            }
+        } else if (A[i] < N) {
+            for (int j = 0; j < M; j++) {
+                A[i] += d;
+                if (A[i] >= N) break;
+                block[j].insert(A[i]);
+            }
+        } else {
+            // ignore
+        }
     }
 
     rep(j,M) {
-        for (int x : in[j]) concerned.insert(x);
-
-        set<int> block;
-        for (int i : concerned) {
-            block.insert(A[i] + (i + 1) * (j + 1));
-        }
-        rep(x,N + 1) if (block.count(x) == 0) {
+        rep(x,N+1) {
+            if (block[j].count(x)) continue;
             cout << x << endl;
             break;
         }
-
-        for (int x : out[j]) concerned.erase(x);
     }
 }
