5f3ebaecf7e0fd29dbcc1f12907552507f15e40e
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-10-15 13:24:36 +0900

    [add] abs rust

--- rust/abs/src/bin/abc085b.rs
+++ rust/abs/src/bin/abc085b.rs
@@ -1,3 +1,16 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: usize,
+        mut d: [i32; n],
+    }
+    d.sort();
+    let mut ans: u32 = 1;
+    for i in 1..n {
+        if d[i] != d[i - 1] {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
