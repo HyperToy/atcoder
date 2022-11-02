35e9d705f4c642544df34f94de71b37e0d9de9c9
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-11-02 09:20:29 +0900

    [update] refactor abc274_d rust

--- rust/abc274/src/bin/d.rs
+++ rust/abc274/src/bin/d.rs
@@ -6,38 +6,20 @@ fn main() {
         n: usize, x: i32, y: i32,
         a: [i32; n],
     }
-    let mut even: Vec<i32> = Vec::new();
-    let mut odd: Vec<i32> = Vec::new();
+    let mut dp = vec![HashSet::new(); 2];
+    dp[0].insert(a[0]);
+    dp[1].insert(0);
     for i in 1..n {
-        if i % 2 == 0 {
-            even.push(a[i]);
-        } else {
-            odd.push(a[i]);
+        let mut next_dp = HashSet::new();
+        for y in &dp[i % 2] {
+            next_dp.insert(y + a[i]);
+            next_dp.insert(y - a[i]);
         }
-    }
-    let mut even_dp = HashSet::new();
-    even_dp.insert(a[0]);
-    let mut odd_dp = HashSet::new();
-    odd_dp.insert(0);
-    for x in even {
-        let mut next_even_dp = HashSet::new();
-        for y in even_dp {
-            next_even_dp.insert(y + x);
-            next_even_dp.insert(y - x);
-        }
-        even_dp = next_even_dp;
-    }
-    for x in odd {
-        let mut next_odd_dp = HashSet::new();
-        for y in odd_dp {
-            next_odd_dp.insert(y + x);
-            next_odd_dp.insert(y - x);
-        }
-        odd_dp = next_odd_dp;
+        dp[i % 2] = next_dp;
     }
     println!(
         "{}",
-        if even_dp.contains(&x) && odd_dp.contains(&y) {
+        if dp[0].contains(&x) && dp[1].contains(&y) {
             "Yes"
         } else {
             "No"
