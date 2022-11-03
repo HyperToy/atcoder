0d5c563ed962b72496f87dd8d1504760fb5bf3b9
Ryohey <ryohey.trumpet+tech@gmail.com>
2022-11-04 08:10:34 +0900

    [upsolve] abc273_e git ぽいやつ

--- abc/273/e.cpp
+++ abc/273/e.cpp
@@ -3,49 +3,37 @@ using namespace std;
 using ll = long long;
 #define rep(i,n) for (int i = 0; i < (n); ++i)
 
+struct Node {int value, parent; };
+
 int main(){
     int Q;
     cin >> Q;
-    vector<string> s(Q);
-    vector<int> a(Q, 0);
-    map<int, int> load;
-    int del = 0;
-    rep(i,Q) {
-        cin >> s[i];
-        if (s[i] != "DELETE") cin >> a[i];
-        else del++;
-        if (s[i] == "LOAD") load[a[i]]++;
-    }
+    map<int, int> note;
+    vector<Node> nodes;
 
-    deque<int> A;
-    map<int, deque<int>> note;
-    rep(i,Q) {
-        if (s[i] == "ADD") {
-            A.push_back(a[i]);
-            while (A.size() > del + 1) {
-                A.pop_front();
-            }
-        } else if (s[i] == "DELETE") {
-            if (A.size() > 0) A.pop_back();
-            del--;
-        } else if (s[i] == "SAVE") {
-            // map アクセス: O(log|note|)
-            // コピー: O(|A|)
-            if (load.count(a[i]) && load[a[i]] > 0) {
-                note[a[i]] = A;
-            }
-        } else if (s[i] == "LOAD") {
-            // map アクセス: O(log|note|)
-            // コピー: O(|note[a[i]]|)
-            A = note[a[i]];
-            load[a[i]]--;
-            // if (load[a[i]] == 0) load.erase(a[i]);
-        } else {
-            // unreachable!();
+    int now = 0;
+    nodes.push_back(Node{-1, 0});
+
+    string s;
+    int x;
+    while (Q--) {
+        cin >> s;
+        if (s == "ADD") {
+            cin >> x;
+            nodes.push_back(Node{x, now});
+            now = (int)nodes.size() - 1;
         }
-        if (A.size() > 0) cout << A.back();
-        else cout << -1;
-        if (i == Q - 1) cout << endl;
-        else cout << " ";
+        if (s == "DELETE") {
+            now = nodes[now].parent;
+        }
+        if (s == "SAVE") {
+            cin >> x;
+            note[x] = now;
+        }
+        if (s == "LOAD") {
+            cin >> x;
+            now = note[x];
+        }
+        cout << nodes[now].value << endl;
     }
 }
